bb3dc76780a0608ba61aa6ff0e5bc425656ec31e
Muhammad Umair Tahir <theumairtahir@gmail.com>
2023-09-17 19:36:23 +0500

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./BlazorHypiness/wwwroot/**/*.{html,htm,js,css}", "./BlazorHypiness/Components/**/*.razor", 
+  "./src/css/**/*.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
