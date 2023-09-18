703e6fcc677fe1a90c4031a0c1a2f062b0b4c6c4
Umair Tahir <theumairtahir@gmail.com>
2023-09-18 19:09:48 +0500

    added text classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./BlazorHypiness/wwwroot/**/*.{html,htm,js,css}", "./BlazorHypiness/Components/**/*.razor", 
   "./src/css/**/*.css"],
   theme: {
+    colors: {
+      'primary': '#1fb6ff',
+    },
+    fontFamily: {
+      base: ['Inter', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
