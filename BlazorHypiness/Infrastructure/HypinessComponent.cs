66d25fe4e35421962a27b4c928af069e7c39b1fc
Umair Tahir <theumairtahir@gmail.com>
2023-09-19 17:45:52 +0500

    changed logic in Hypiness Component

--- BlazorHypiness/Infrastructure/HypinessComponent.cs
+++ BlazorHypiness/Infrastructure/HypinessComponent.cs
@@ -4,5 +4,15 @@ using Microsoft.AspNetCore.Components;
 namespace BlazorHypiness.Infrastructure;
 public abstract class HypinessComponent : ComponentBase
 {
-	protected virtual string BuildCss(CssBuilder builder) => builder.Build( );
+	private string? compiledCss;
+
+	protected string CompiledCss { get => compiledCss ??= BuildCss(new CssBuilder( )).Build( ); }
+
+	protected override void OnParametersSet( )
+	{
+		compiledCss = null;
+		base.OnParametersSet( );
+	}
+
+	protected virtual CssBuilder BuildCss(CssBuilder builder) => builder;
 }
