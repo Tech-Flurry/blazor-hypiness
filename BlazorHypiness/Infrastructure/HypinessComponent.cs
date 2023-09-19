c1438c74c74287d2127f517f9e674bba7631e405
Umair Tahir <theumairtahir@gmail.com>
2023-09-19 17:08:37 +0500

    added hypiness base component

---
+++ BlazorHypiness/Infrastructure/HypinessComponent.cs
@@ -0,0 +1,8 @@
+﻿using BlazorHypiness.Utilities;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorHypiness.Infrastructure;
+public abstract class HypinessComponent : ComponentBase
+{
+	protected virtual string BuildCss(CssBuilder builder) => builder.Build( );
+}
