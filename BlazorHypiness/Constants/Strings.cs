2e4ccc0201f8312e0d9c4e6972fd75dc0c3d3303
Umair Tahir <theumairtahir@gmail.com>
2023-09-28 13:52:03 +0500

    added modifications to HypinessComponent

--- BlazorHypiness/Constants/Strings.cs
+++ BlazorHypiness/Constants/Strings.cs
@@ -4,4 +4,5 @@ internal static class Strings
 {
 	public const string EMPTY_STRING = "";
 	public const string CLASS = "class";
+	public const string STYLE = "style";
 }
--- BlazorHypiness/Infrastructure/HypinessComponent.cs
+++ BlazorHypiness/Infrastructure/HypinessComponent.cs
@@ -1,18 +1,35 @@
-﻿using BlazorHypiness.Utilities;
+﻿using BlazorHypiness.Constants;
+using BlazorHypiness.Utilities;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazorHypiness.Infrastructure;
 public abstract class HypinessComponent : ComponentBase
 {
 	private string? compiledCss;
+	private readonly Dictionary<string, object> _inputAttributes = new( );
 
+	protected IReadOnlyDictionary<string, object> InputAttributes => _inputAttributes;
 	protected string CompiledCss { get => compiledCss ??= BuildCss(new CssBuilder( )).Build( ); }
+
+	[Parameter]
+	public string? CssClass { get; set; }
+
+	[Parameter]
+	public string? Style { get; set; }
 
 	protected override void OnParametersSet( )
 	{
 		compiledCss = null;
+		_inputAttributes.Clear( );
 		base.OnParametersSet( );
+		if (Style is not null && !_inputAttributes.ContainsKey(Strings.STYLE))
+			_inputAttributes.Add(Strings.STYLE, Style);
 	}
 
-	protected virtual CssBuilder BuildCss(CssBuilder builder) => builder;
+	protected virtual CssBuilder BuildCss(CssBuilder builder)
+	{
+		if (CssClass is not null)
+			builder.Add(CssClass);
+		return builder;
+	}
 }
